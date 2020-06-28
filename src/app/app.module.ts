ea4b85542324ee7db0c7c5ac2eac50a54ecfd397
varsha <varsha.y@elmeasure.com>
2020-06-28 21:25:21 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
